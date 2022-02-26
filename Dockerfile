d11464d06bc3083431ef874260708749473988a8
EC2 Default User <ec2-user@ip-172-31-82-44.ec2.internal>
2022-02-26 13:08:29 +0000

    Updated setenv.sh

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM tomcat
 RUN ["wget","-O","dd-java-agent.jar","https://dtdg.co/latest-java-tracer"]
 
 COPY sample.war /usr/local/tomcat/webapps/
+
+COPY setenv.sh /usr/local/tomcat/bin/
  
 
 EXPOSE 8080
--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,1 @@
-export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/dd-java-agent.jar"
+export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/dd-java-agent.jar -Ddd.logs.injection=true -Ddd.service=tomcat -Ddd.env=lab" 
